654a4bb187cc3f277dc041f7e8def491af27013b
JordyAM <JAlexanderMcLean@Gmail.com>
2023-03-03 02:19:41 -0500

    updating porfolio

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2d70555d8705870e"; }
+/******/ 		__webpack_require__.h = function() { return "e253aaa682de5c56"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,4 +1,5 @@
 import { motion as m } from "framer-motion";
+import { container, item } from "animation";
 
 export default function Contact(){
     return (
@@ -8,18 +9,26 @@ export default function Contact(){
         transition= {{ duration: 0.75, ease: "easeOut" }}
         exit={{ opacity: 1 }} 
         className="text-gray-900 absolute top-0 left-0 w-full h-full bg-red-400 lg:px-48 px-16">
-            <div className="my-96 p-1">
-                <h1 className="text-6xl text-center lg:text-right lg:text-9xl">Lets' Talk</h1>
+            <div className="my-96 p-1 overflow-hidden">
+                <m.h1 
+                animate={{ y: 0 }}
+                initial={{ y: "100%" }} 
+                transition={{ delay: 0.5, duration: 0.5 }}
+                className="text-6xl text-center lg:text-right lg:text-9xl">Lets' Talk</m.h1>
             </div>
             <div className="flex gap-40">
                 <div className="lg:text-2xl text-base">
                     <h4>Find Me:</h4>
                 </div>
                 <div className="lg:text-6xl text-2xl underline">
-                    <ul>
-                        <li className="pb-2">LinkedIn</li>
-                        <li className="pb-2">GitHub</li>
-                    </ul>
+                    <m.ul variants={container} initial='hidden' animate='show'>
+                        <div className="overflow-hidden">
+                            <m.li variants={item} className="pb-2">LinkedIn</m.li>
+                        </div>
+                        <div className="overflow-hidden">
+                            <m.li variants={item}className="pb-2">GitHub</m.li>
+                        </div>
+                    </m.ul>
                 </div>
             </div>
         </m.main>
