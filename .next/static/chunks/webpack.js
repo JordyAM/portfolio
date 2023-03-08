dd8f7c6218612c2c9872fd677f3fcdd812c5b743
JordyAM <JAlexanderMcLean@Gmail.com>
2023-03-08 12:22:08 -0500

    fixing links

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "12df089d718f2431"; }
+/******/ 		__webpack_require__.h = function() { return "d6aa8ca54431b660"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -19,11 +19,11 @@ export default function projects (){
             <div>
            <h1>Here are a few projects I've worked on!</h1>
            <div className="site-container"><h1>A Final Fantasy XIV Character Viewer</h1>
-           <img className='project-image' src="FFXIVViewer.png"></img>
+           <img className='object-contain h-96 w-96' src="FFXIVViewer.png"></img>
            <a className="project-url" href="https://github.com/JordyAM/ffxiv-viewer"><h3>Code & Docs</h3></a>
-           <a className="project-url" href="https://peaceful-williams-d162bf.netlify.app/"><h3>App</h3></a></div>
+           <a className="project-url" href="https://ffxiv-viewer.netlify.app/"><h3>App</h3></a></div>
            <div className="site-container"><h1>Collectus API</h1>
-           <img className='project-image' src="Collectus.png"></img>
+           <img className='object-contain h-96 w-96' src="Collectus.png"></img>
            <a className="project-url" href="https://github.com/Team-This-Is-Fine/collectus-frontend"><h3>Code & Docs</h3></a>
            <a className="project-url" href="https://animated-pony-a34044.netlify.app/"><h3>App</h3></a></div>
         </div>
