a74104b46a0ca18c710fb24f54b124881d7c2a2e
JordyAM <JalexanderMcLean@Gmail.com>
2023-08-14 12:19:59 -0500

    update time

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -19,13 +19,16 @@ export default function projects (){
             <div>
            <h1>Here are a few projects I've worked on!</h1>
            <div className="site-container"><h1>A Final Fantasy XIV Character Viewer</h1>
-           <img className='object-contain h-96 w-96' src="FFXIVViewer.png"></img>
-           <a className="project-url" href="https://github.com/JordyAM/ffxiv-viewer"><h3>Code & Docs</h3></a>
-           <a className="project-url" href="https://ffxiv-viewer.netlify.app/"><h3>App</h3></a></div>
-           <div className="site-container"><h1>Collectus API</h1>
-           <img className='object-contain h-96 w-96' src="Collectus.png"></img>
-           <a className="project-url" href="https://github.com/Team-This-Is-Fine/collectus-frontend"><h3>Code & Docs</h3></a>
-           <a className="project-url" href="https://animated-pony-a34044.netlify.app/"><h3>App</h3></a></div>
+            <img className='object-contain h-96 w-96' src="FFXIVViewer.png"></img>
+            <a className="project-url" href="https://github.com/JordyAM/ffxiv-viewer"><h3>Code & Docs</h3></a>
+            <a className="project-url" href="https://ffxiv-viewer.netlify.app/"><h3>App</h3></a>
+            </div>
+           <div className="site-container">
+            <h1>Collectus API</h1>
+            <img className='object-contain h-96 w-96' src="Collectus.png"></img>
+            <a className="project-url" href="https://github.com/Team-This-Is-Fine/collectus-frontend"><h3>Code & Docs</h3></a>
+            <a className="project-url" href="https://animated-pony-a34044.netlify.app/"><h3>App</h3></a>
+            </div>
         </div>
         </m.main>
     )
